23e3239fccbf6a2da7db4ae4786ae9f1e59b715d
JuanSeDelgado <73569662+JuanSeDelgado@users.noreply.github.com>
2025-04-09 15:48:45 +0000

    First Commit Excuses Generator

--- src/app.js
+++ src/app.js
@@ -1,11 +1,64 @@
-import "bootstrap";
-import "./style.css";
+// This is the excuse generator
+let who = [
+  'The dog',
+  'My grandma',
+  'The mailman',
+  'My bird',
+  'A random raccoon',
+  'My neighbor',
+  'My little brother',
+  'An alien',
+  'My cat',
+  'The ghost in my house'
+];
 
+let action = [
+  'ate',
+  'peed on',
+  'crushed',
+  'broke',
+  'deleted',
+  'hid',
+  'set fire to',
+  'threw away',
+  'painted over',
+  'teleported'
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let what = [
+  'my homework',
+  'my phone',
+  'the car',
+  'my shoes',
+  'the TV remote',
+  'my favorite book',
+  'my school project',
+  'the computer',
+  'the kitchen table',
+  'the fridge'
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let when = [
+  'before the class',
+  'when I was sleeping',
+  'while I was exercising',
+  'during my lunch',
+  'while I was praying',
+  'right after dinner',
+  'in the middle of the night',
+  'when I went to the bathroom',
+  'just before I left the house',
+  'while I was feeding the cat'
+];
+
+function generateExcuse() {
+  let excuse = 
+    who[Math.floor(Math.random() * who.length)] + ' ' +
+    action[Math.floor(Math.random() * action.length)] + ' ' +
+    what[Math.floor(Math.random() * what.length)] + ' ' +
+    when[Math.floor(Math.random() * when.length)] + '.';
+
+    document.getElementById('excuse').innerText = excuse;
+    console.log(excuse);
+    return excuse;
+}
